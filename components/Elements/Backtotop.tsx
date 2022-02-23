f93ed3c3903396c47e28a4afe2f99f00d91a381c
Maestroharyor <haryor1josh@gmail.com>
2022-02-23 17:03:18 +0100

    Edits icon type

--- components/Elements/Backtotop.tsx
+++ components/Elements/Backtotop.tsx
@@ -2,9 +2,12 @@ import {MdArrowUpward} from 'react-icons/md';
 import { BackTop } from 'antd';
 
 const Backtotop = () => {
+    const UpArrow:any = MdArrowUpward
     return(
         <BackTop>
-            <div className="flex items-center justify-center bg-primary text-white hover:bg-primary-hov dark:bg-warning dark:text-dark dark:hover:bg-warning-hov p-3 rounded-full shadow text-lg"><MdArrowUpward /></div>
+            <div className="flex items-center justify-center bg-primary text-white hover:bg-primary-hov dark:bg-warning dark:text-dark dark:hover:bg-warning-hov p-3 rounded-full shadow text-lg">
+                <UpArrow />
+                </div>
         </BackTop>
     )
 }
